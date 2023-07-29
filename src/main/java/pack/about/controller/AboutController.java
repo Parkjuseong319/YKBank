e1f27c38736baa40b4d647d632b44d0025aaed7b
Parkjuseong319 <wntjd03199@gmail.com>
2023-07-30 00:42:56 +0900

    ykbank

--- src/main/java/pack/about/controller/AboutController.java
+++ src/main/java/pack/about/controller/AboutController.java
@@ -1,9 +1,5 @@
 package pack.about.controller;
 
-import java.util.ArrayList;
-
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
